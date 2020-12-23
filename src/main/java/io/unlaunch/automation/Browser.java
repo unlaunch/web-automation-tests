8c87e930dce689ddac41decdb380ce327c1214e1
Fahmina Ghauri <fghauri@crovateoffshore.com>
2020-12-24 03:08:01 +0500

    add api host name for sdktest

--- src/main/java/io/unlaunch/automation/Browser.java
+++ src/main/java/io/unlaunch/automation/Browser.java
@@ -25,6 +25,7 @@ public class Browser {
 
         System.setProperty("webdriver.chrome.driver", settings.getWebDriverLocation());
         hostname = settings.getHostname();
+        apiHostname = settings.getApiHostname();
         logoutUrl = settings.getHostname() + "/logout";
         clearInputFieldKeySequence = settings.getClearInputFieldSequence();
     }
@@ -34,7 +35,7 @@ public class Browser {
     public static final String emailAddress = "unlaunch.test+" + UUID.randomUUID().toString() + "@gmail.com";
     public static WebDriver driver = new ChromeDriver(getChromeOptions());
     public static final String logoutUrl;
-
+    public static final String apiHostname;
     public static void goTo(String url) {
         driver.get(url);
     }
